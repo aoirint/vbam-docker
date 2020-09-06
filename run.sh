a27bfb5920d7e6a7054de5880eb5147b69ab5dde
aoirint <aoirint@gmail.com>
2020-09-06 19:15:47 +0900

    mount sound device & host dir

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo docker run -it --rm --name vbam -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix vbam visualboyadvance-m
+sudo docker run -it --rm --name vbam \
+  -e DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --device /dev/snd \
+  -v $PWD/vbam:/vbam \
+  vbam \
+  visualboyadvance-m
